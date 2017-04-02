dcde79a22ab1ef93e31d9475d328371054e36a56
RecursiveLogic <luis@radiux.io>
2017-04-02 06:04:21 -0400

    Refactored code to be more abstract

--- utils/yFinance.js
+++ utils/yFinance.js
@@ -4,37 +4,36 @@ const bhttp = require('bhttp')
 const baseURL = 'http://query.yahooapis.com/v1/public/yql?q='
 const tailURL = '&format=json&env=store://datatables.org/alltableswithkeys'
 
+const getYFinanceData = (query) => {
+  return Promise.try(() => {
+    return bhttp.get(query)
+  }).then(data =>
+    data.body.query.results
+  )
+}
+
 module.exports = {
   getQuote: (symbols) => {
     const buffer = Array.isArray(symbols) ? symbols.join(',') : symbols
     const YQL = `SELECT * FROM yahoo.finance.quotes WHERE symbol IN ('${buffer}')`
+    const query = `${baseURL}${YQL}${tailURL}`
 
-    return Promise.try(() => {
-      return bhttp.get(`${baseURL}${YQL}${tailURL}`)
-    }).then(data =>
-      data.body.query.results
-    )
+    return getYFinanceData(query)
   },
   getHistory: (symbols, start, end) => {
     const buffer = Array.isArray(symbols) ? symbols.join(',') : symbols
     const YQL = `SELECT * FROM yahoo.finance.historicaldata WHERE symbol = ` +
       `'${buffer}' AND startDate = '${start}' AND endDate = '${end}'`
+    const query = `${baseURL}${YQL}${tailURL}`
 
-    return Promise.try(() => {
-      return bhttp.get(`${baseURL}${YQL}${tailURL}`)
-    }).then(data =>
-      data.body.query.results
-    )
+    return getYFinanceData(query)
   },
   getDividendHistory: (symbol, start, end) => {
     const buffer = Array.isArray(symbols) ? symbols.join(',') : symbols
     const YQL = `SELECT * FROM yahoo.finance.dividendhistory WHERE symbol = ` +
       `'${buffer}' AND startDate = '${start}' AND endDate = '${end}'`
+    const query = `${baseURL}${YQL}${tailURL}`
 
-    return Promise.try(() => {
-      return bhttp.get(`${baseURL}${YQL}${tailURL}`)
-    }).then(data =>
-      data.body.query.results
-    )
+    return getYFinanceData(query)
   }
 }
