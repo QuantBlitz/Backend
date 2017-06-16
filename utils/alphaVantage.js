ea9469a590e04b982d6c253687677d159c0ef70a
QL <qrx9823@gmail.com>
2017-06-16 19:26:19 -0400

    Added utils for interfacing with AlphaVantage API

---
+++ utils/alphaVantage.js
@@ -0,0 +1,22 @@
+const Promise = require('bluebird')
+const bhttp = require('bhttp')
+
+const { alpha_vantage } = require('../api_keys.json')
+
+const baseURL = 'http://www.alphavantage.co/query?function='
+const tailURL = '&apikey=' + alpha_vantage
+
+const getAVData = (query) => {
+  return Promise.try(() =>
+    bhttp.get(query)
+  ).then(data =>
+    data.body
+  )
+}
+
+module.exports = {
+  getChartDaily: (symbol) => {
+    const query = `${baseURL}TIME_SERIES_DAILY&symbol=${symbol}${tailURL}`
+    return getAVData(query)
+  }
+}
